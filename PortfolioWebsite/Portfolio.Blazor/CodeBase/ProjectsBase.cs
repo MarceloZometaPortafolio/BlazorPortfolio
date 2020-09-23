6b00515d008d87e9eb4c51670c328aefd86f9882
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-09-23 02:15:05 -0600

    cant call api on wasm, weird null exception

---
+++ PortfolioWebsite/Portfolio.Blazor/CodeBase/ProjectsBase.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Components;
+using Portfolio.Blazor.DataProvider;
+using Portfolio.Blazor.Pages;
+using Portfolio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Portfolio.Blazor.CodeBase
+{
+    public class ProjectsBase : ComponentBase
+    {
+        //private APIService service;
+        public bool isAddComponentDisplayed = false;
+
+        //protected override Task OnInitializedAsync()
+        //{
+        //    this.service = service;
+        //    return base.OnInitializedAsync();
+        //}
+
+        //public ProjectsBase()
+        //{
+        //    this.service = service ?? throw new ArgumentNullException(nameof(service));
+        //}
+        
+        public string PageTitle { get; set; }
+
+        public IEnumerable<Project> Projects { get; private set; }
+
+        public void EditProject(int id, APIService service)
+        { 
+        }
+
+        public async Task DeleteProject(int id, APIService service)
+        {
+            await service.DeleteProjectAsync(id);
+        }
+
+        public void ShowAddComponent()
+        {
+            if(isAddComponentDisplayed is false)
+            {
+                isAddComponentDisplayed = true;
+            }
+            else
+            {
+                isAddComponentDisplayed = false;
+            }
+        }        
+    }
+}
---
+++ PortfolioWebsite/Portfolio.Blazor/DataProvider/APIService.cs
@@ -0,0 +1,44 @@
+﻿using Portfolio.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Portfolio.Blazor.DataProvider
+{
+    public class APIService
+    {
+        private readonly HttpClient client;
+
+        public APIService(HttpClient client)
+        {
+            this.client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<IEnumerable<Project>> GetProjectsAsync()
+        {
+            var projects = await client.GetFromJsonAsync<IEnumerable<Project>>("api/project");
+
+            return projects;
+        }
+
+        public async Task AddProjectAsync(Project project)
+        {
+            await client.PostAsJsonAsync("api/project", project);
+        }
+
+        public async Task<Project> GetProjectByIDAsync(int id)
+        {
+            var project = await client.GetFromJsonAsync<Project>($"api/project/{id}");
+
+            return project;
+        }
+
+        public async Task DeleteProjectAsync(int id)
+        {
+            await client.DeleteAsync($"api/project/{id}");
+        }
+    }
+}
