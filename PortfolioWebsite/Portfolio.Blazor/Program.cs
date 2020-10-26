91c23dc2731b8a3b77251533ebc13c6641ceba3d
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-10-26 15:44:17 -0600

    Now I can watch the blazor run

--- PortfolioWebsite/Portfolio.Blazor/Program.cs
+++ PortfolioWebsite/Portfolio.Blazor/Program.cs
@@ -27,7 +27,7 @@ namespace Portfolio.Blazor
                 .SetHandlerLifetime(TimeSpan.FromMinutes(5))
                 .AddPolicyHandler(GetPolicy());
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            //builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             builder.Services.AddOidcAuthentication(options =>
             {
