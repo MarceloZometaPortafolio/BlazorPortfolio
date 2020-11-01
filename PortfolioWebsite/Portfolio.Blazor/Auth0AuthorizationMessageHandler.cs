2ba5d957ae99ab07feba6fc5a8fc7f5c4bfb1d9e
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-10-31 21:47:33 -0600

    you can see languages again

--- PortfolioWebsite/Portfolio.Blazor/Auth0AuthorizationMessageHandler.cs
+++ PortfolioWebsite/Portfolio.Blazor/Auth0AuthorizationMessageHandler.cs
@@ -13,7 +13,7 @@ namespace Portfolio.Blazor
         public Auth0AuthorizationMessageHandler(IAccessTokenProvider provider, NavigationManager navigationManager, IConfiguration config) : base(provider, navigationManager)
         {
             this.ConfigureHandler(
-                authorizedUrls: new[] { "https://localhost:5001", "https://localhost:5005", "https://portfolio-api-marcelo.herokuapp.com/", "https://marcelozometaportafolio.github.io/BlazorPortfolio/" }
+                authorizedUrls: new[] {config["APIBaseAddress"], "https://localhost:5001", "https://localhost:5005", "https://portfolio-api-marcelo.herokuapp.com/", "https://marcelozometaportafolio.github.io/BlazorPortfolio/", "https://dev--qt6q8jb.us.auth0.com" }
             );
         }
     }
