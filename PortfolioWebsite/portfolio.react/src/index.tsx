bb04a648a780bc46c77094ea1ab38aa5bee0a1b2
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-11-10 21:43:46 -0700

    project loads perfectly

--- PortfolioWebsite/portfolio.react/src/index.tsx
+++ PortfolioWebsite/portfolio.react/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Main from './components/Main';
+import Blog from './components/Blog';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>,
   document.getElementById('root')
 );
