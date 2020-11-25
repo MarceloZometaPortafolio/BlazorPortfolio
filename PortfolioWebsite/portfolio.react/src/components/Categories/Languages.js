f27d8b8201209f259a4a3d08572672608e2b1fa9
Marcelo Zometa <marcelo.zometa@hotmail.com>
2020-11-25 02:58:02 -0700

    langauges work perfectly

--- PortfolioWebsite/portfolio.react/src/components/Categories/Languages.js
+++ PortfolioWebsite/portfolio.react/src/components/Categories/Languages.js
@@ -1,18 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { Card, CardContent, CardHeader } from '@material-ui/core';
 import APIService from '../../Data/APIService'
-import { makeStyles } from '@material-ui/core/styles';
 import CategoryCard from '../Projects/CategoryCard';
-
-const useStyles = makeStyles((theme) => ({
-    leftAligned: {
-      textAlign : "left"
-    },
-  }));
 
 const Languages = (props) => {              
     const [languages, setLanguages] = useState([]);        
-    const classes = useStyles();
 
     const getLanguages = async() => {
         const languages = await APIService.getLanguages();
@@ -32,13 +24,9 @@ const Languages = (props) => {
             <Card>
                 <CardHeader title={"Languages"}/>
                 <CardContent>                    
-                {languages.map(c => 
-               
-               <ul key={c.id}>
-                   <li className={classes.leftAligned}>{c.name}</li>
-               </ul>
+                    <CategoryCard category={languages}/>
           
-       )}
+       
                 </CardContent>
             </Card>
         </div>
